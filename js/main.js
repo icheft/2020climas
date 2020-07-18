8d6f4b2f81f92964e4ad6c27a4faa9ca82765438
Brian L. Chen <brian.lxchen@gmail.com>
2020-07-18 10:53:06 +0800

    Add shirt size

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@
         });
     });
 
-    //to be changed: so hard~~~~idk how to write JS
     function getCountry(){
         //  讀取radio的值
         var togetherGo = document.getElementById("go-together");
@@ -71,38 +70,3 @@
 })(jQuery);
 
 
-
-// light/dark toggle
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-const isDarkModeOn = document.querySelector('#dark-mode-prompt');
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
-        $("#form-btn").removeClass('btn-outline-dark').addClass('btn-outline-light');
-        isDarkModeOn.innerHTML = `Dark Mode Enabled`;
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); //add this
-        $("#form-btn").removeClass('btn-outline-light').addClass('btn-outline-dark');
-
-        isDarkModeOn.innerHTML = `Light Mode On`;
-    }    
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        $("#form-btn").removeClass('btn-outline-dark').addClass('btn-outline-light');
-        isDarkModeOn.innerHTML = `Dark Mode Enabled`;
-    }
-}
---
+++ js/popup.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+  });
+  
